817a43d337615d70daae2d74f6ce4358e1e10e93
Axel Fouli <oleksh96@gmail.com>
2020-10-24 16:33:25 +1000

    Show articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,19 @@
+
 class ArticlesController < ApplicationController
+
+    def index
+        @articles = Article.all
+    end
+
+    def show
+        @article = Article.find(params[:id])
+    end
+
     def new
     end
 
     def create
-    @article = Aritcle.new(article_params)
+    @article = Article.new(article_params)
 
     @article.save
     redirect_to @article
